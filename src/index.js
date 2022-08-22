b9ce2d42878f157c4cd5d5d5a59a1845e10e3e3c
manisha sinha <bits.manisha@gmail.com>
2022-08-22 21:52:47 +0200

    Changed H1 Content

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import axios from 'axios';
 
 const rootDOM = document.getElementById('root');
 const appRoot = ReactDOM.createRoot(rootDOM);
+
+
+
+
 
 appRoot.render(
     <h1>My refresher app</h1>,
