67da3f725c240af2b339361a9e51a10dc8999db9
Svetlana <svk3004@gmail.com>
2022-08-22 15:12:09 +0200

    Added App component

---
+++ src/App.js
@@ -0,0 +1,13 @@
+function FirstComponent () {
+    return (
+        <div className="NavBar">
+            <header className="Header">
+                <h1>My React</h1>
+                <p>text</p>
+                <img src="./img.jpg" alt="image"></img>
+            </header>
+        </div>
+    )
+}
+
+export default FirstComponent;
