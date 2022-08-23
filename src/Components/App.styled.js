c0787e5176344fc122b91dea5b3a22e4d269c2da
Tetre101 <basilpeter989@gmail.com>
2022-08-23 09:18:41 +0200

    basile

---
+++ src/Components/App.styled.js
@@ -0,0 +1,58 @@
+import styled from "@emotion/styled";
+
+
+export const MainContainer = styled.div`
+`;
+export const PictureContainer = styled.div`
+width: 100%
+color:blue;
+text-align: center;
+margin: auto;
+display: flex;
+column-gap: 10px;
+row-gap: 10px;
+flex-wrap: wrap;
+overflow:hidden;
+img:hover{width:30vw;}
+
+`;
+
+export const TextBoxStyled = styled.div`
+width: 100%
+color:blue;
+text-align: center;
+margin: auto;
+display: flex;
+column-gap: 10px;
+row-gap: 10px;
+flex-wrap: wrap;
+overflow:hidden;
+`;
+
+export const BoxPicture = styled.img`
+width: 20vw;
+height: 20vw;
+margin-top:2vw;
+margin-bottom:2vw;
+margin-left: auto;
+margin-right: auto;
+
+`;
+export const BoxTextContainerStyled = styled.div`
+border: solid red;
+width: 20vw;
+height: 20vw;
+margin-top:5vw;
+margin-bottom:5vw;
+text-align: center;
+margin-left: auto;
+margin-right: auto;
+
+`;
+
+export const Text = styled.p`
+
+`
+export const Bold = styled.h1`
+
+`;
